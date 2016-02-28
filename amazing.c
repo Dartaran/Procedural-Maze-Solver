4c219311cb171de73d37d8feb5f74a8edf33f7c0
Kyu Hyeon Kim <Kyu.Hyeon.Kim.18@dartmouth.edu>
2016-02-28 15:58:32 -0500

    Started amazing.c

---
+++ amazing.c
@@ -0,0 +1,31 @@
+/*	amazing.c Maze solving program
+
+	Project name: CS50 Amazing Project
+	Component name: amazing program
+
+	Primary Author:	Kyu Kim, Alan Lu, Donald Wilson (Lambrusco)
+	Date Created:	2/28/16
+
+	Special considerations:  
+		(e.g., special compilation options, platform limitations, etc.) 
+	
+======================================================================*/
+// do not remove any of these sections, even if they are empty
+//
+// ---------------- Open Issues 
+
+// ---------------- System includes e.g., <stdio.h>
+
+// ---------------- Local includes  e.g., "file.h"
+
+// ---------------- Constant definitions 
+
+// ---------------- Macro definitions
+
+// ---------------- Structures/Types 
+
+// ---------------- Private variables 
+
+// ---------------- Private prototypes 
+
+/*====================================================================*/
